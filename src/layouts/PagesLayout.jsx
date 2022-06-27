e9830553d65aea5d6964acd0055fd66fbdbdf058
cdguedez <cdgr.ing@gmail.com>
2022-06-27 00:40:16 -0400

    [FEATURE] add array project

--- src/layouts/PagesLayout.jsx
+++ src/layouts/PagesLayout.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const PagesLayout = ({ children }) => {
   return (
-    <main className='bg-home bg-cover h-screen flex justify-center items-center text-slate-50 px-4'>
-      <section className='w-full bg-black bg-opacity-50 px-6 py-4 rounded-lg text-center'>
-        {children}
-      </section>
-    </main>
+    <>
+      <main className='bg-black bg-center min-h-screen h-auto flex justify-center items-center text-slate-50 p-4 relative top-[70px]'>
+        <section className='w-full bg-blue-200 bg-opacity-10 px-6 py-4 rounded-lg text-center'>
+          {children}
+        </section>
+      </main>
+    </>
   )
 }
 
