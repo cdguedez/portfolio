44de6c82810cce03ac29281bcf03e4b41d8b5552
cdguedez <cdgr.ing@gmail.com>
2022-06-26 20:57:53 -0400

    [DEPLOY] first deploy

---
+++ dist/assets/Contact.07a2621f.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index.f569d5cc.js";const r=()=>t("div",{children:"Contact"});export{r as default};
---
+++ dist/assets/Portfolio.2e2cd5a9.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index.f569d5cc.js";const t=()=>e("div",{className:"h-screen flex justify-center items-center",children:"gr"});export{t as default};
