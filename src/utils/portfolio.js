9c03035bfd36552b5a7e95cfed1c9bad4b6d4a4b
cdguedez <cdgr.ing@gmail.com>
2022-06-27 10:39:00 -0400

    [FIX] fix of images in repo

--- src/utils/portfolio.js
+++ src/utils/portfolio.js
@@ -3,7 +3,7 @@ export const projects = [
     id: 'CG01',
     name: 'Efiempresa LLC',
     url: 'https://efiempresa.com/',
-    images: ['/src/assets/images/portfolio/efiempresa-desktop.jpg', '/src/assets/images/portfolio/efiempresa-mobile.jpg'],
+    images: ['https://photos.app.goo.gl/YZuyi4iPkhWVPXM17', 'https://photos.app.goo.gl/uv3qTUXALaSUjRNY8'],
     description: 'Web de Efiempresa realizada con wordpress para mi empleador.',
     isFinish: true,
     skills: ['wordpress', 'elementor', 'CSS']
@@ -12,7 +12,7 @@ export const projects = [
     id: 'CG02',
     name: 'Dra Angela Chamorro',
     url: 'https://www.drangelachamorro.com/',
-    images: ['/src/assets/images/portfolio/drangelachamorro-desktop.jpg', '/src/assets/images/portfolio/drangelachamorro-mobile.jpg'],
+    images: ['https://photos.app.goo.gl/kCa2NfcrMi6UeM557', 'https://photos.app.goo.gl/ENKjBeocsS5Zy9WNA'],
     description: 'Web de Efiempresa realizada con wordpress, con mi anterior empleador Efiempresa LLC.',
     isFinish: true,
     skills: ['wordpress', 'Avada Theme', 'gutenberg page builder', 'CSS', 'JS vanila']
@@ -21,7 +21,7 @@ export const projects = [
     id: 'CG03',
     name: 'Portal de empleos Efiempleo',
     url: 'https://efiempleo.com/',
-    images: ['/src/assets/images/portfolio/efiempleo-desktop.jpg', '/src/assets/images/portfolio/efiempleo-mobile.jpg'],
+    images: ['https://photos.app.goo.gl/3Lgn7EhZXt2YjJir7', 'https://photos.app.goo.gl/mabKBSAQ3C4UMVZV9'],
     description: 'Portal de empleos realizado con wordpress, perteneciante a Efiempresa LLC.',
     isFinish: true,
     skills: ['wordpress', 'woocomerce', 'gutenberg page builder', 'CSS']
@@ -30,7 +30,7 @@ export const projects = [
     id: 'CG04',
     name: 'Credivehiculos',
     url: 'https://credivehiculos.com/',
-    images: ['/src/assets/images/portfolio/credivehiculos-desktop.jpg', '/src/assets/images/portfolio/credivehiculos-mobile.jpg'],
+    images: ['https://photos.app.goo.gl/W6L9UVLswx9urCAYA', 'https://photos.app.goo.gl/PbregoNJFrveGbZK8'],
     description: 'Web realizada en wordpress.',
     isFinish: true,
     skills: ['wordpress', 'elementor', 'CSS']
@@ -39,7 +39,7 @@ export const projects = [
     id: 'CG05',
     name: 'SPA Efitratos',
     url: 'https://app.efitratos.com/',
-    images: ['/src/assets/images/portfolio/app-efitratos-desktop.jpg', '/src/assets/images/portfolio/app-efitratos-mobile.jpg'],
+    images: ['https://photos.app.goo.gl/dn89CGZRUKL159Tc8', 'https://photos.app.goo.gl/KuGWYzCT4hBdFBAt7'],
     description: 'Sistema de gestion de propuestas realizado con el STACK MySQL ExpressVuejs Nodejs',
     isFinish: true,
     skills: ['Nodejs', 'Vuejs', 'expressjs', 'MySql', 'middlewares', '@hapi/boom', 'Sequelize', 'ORM', 'Joi', 'vue bootstrap']
@@ -48,7 +48,7 @@ export const projects = [
     id: 'CG06',
     name: 'Backoffice de contratos',
     url: '#',
-    images: [],
+    images: '',
     description: 'Sistema de contratos realizado con PHP, bootstrapp, javascrip vanila',
     isFinish: true,
     skills: ['CSS', 'JS vanila', 'PHP 7', 'HTML5', 'bootstrap', 'MySql']
@@ -57,7 +57,7 @@ export const projects = [
     id: 'CG07',
     name: 'Pagina de recambios kawasaki',
     url: 'http://recambioskawasakimoto.com/',
-    images: [],
+    images: '',
     description: 'web de recambios realizada en wordpress',
     isFinish: false,
     skills: ['wordpress', 'CSS', 'woocomerce']
@@ -66,21 +66,12 @@ export const projects = [
 
 export const me = {
   name: 'Carlos Guedez',
-  country: 'venezuela',
+  country: 'VENEZUELA',
   language: [
     { id: 'l1', name: 'spanish', level: 'nativo' },
     { id: 'l2', name: 'english', level: 'basic' }
   ],
-  skills: [
-    'wordpress',
-    'CSS',
-    'HTML5',
-    'Linux',
-    'javascript',
-    'Reactjs',
-    'Nodejs',
-    'expressjs'
-  ],
+  skills: ['wordpress', 'CSS', 'HTML5', 'Linux', 'javascript', 'Reactjs', 'Nodejs', 'expressjs'],
   socialLinks: [
     { name: 'twitter', url: 'https://twitter.com/cdguedez' },
     { name: 'linkedin', url: 'https://linkedin.com/in/cdguedez' }
