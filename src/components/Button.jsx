a3827bdb1424f0cfab483938043c14b91d1ad7f0
cdguedez <cdgr.ing@gmail.com>
2022-06-27 18:45:39 -0400

    [FEATURE] add grid in portfolio

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const linkCv = 'https://drive.google.com/file/d/1SLPQZO_ErUD1eItP-GdpBlsBpH_aK6fA/view?usp=sharing'
-
-const Button = ({ children }) => {
+const Button = ({ children, ...props }) => {
   return (
     <button
+      {...props}
       className='bg-slate-900 px-4 py-2 rounded-md text-white text-sm hover:opacity-50 transition-all'
     >
       {children}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,15 @@ import Skills from './../components/Skills'
 import { me } from './../utils/portfolio'
 import Button from './../components/Button'
 
+// const linkCv = 'https://drive.google.com/file/d/1SLPQZO_ErUD1eItP-GdpBlsBpH_aK6fA/view?usp=sharing'
+
 const Home = () => {
+  const handleClick = () => window.alert('resume not available')
+
   return (
-    <>
+    <section className='bg-black bg-opacity-20 p-8 rounded-lg max-w-5xl'>
       <h1 className='uppercase font-bold text-2xl mb-2 text-left'>
-        i'm {me.name} <Button>Download CV</Button>
+        i'm {me.name} <Button onClick={handleClick}>Download CV</Button>
       </h1>
       <h2 className='font-semibold text-lg text-left'>Web developer from {' '}
         <span className='font-bold text-lime-400 uppercase'>
@@ -15,7 +19,7 @@ const Home = () => {
         </span>
       </h2>
       <Skills skills={me.skills} />
-    </>
+    </section>
   )
 }
 
