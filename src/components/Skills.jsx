9bd73fdccab3398fe257660a728d8f00be380693
cdguedez <cdgr.ing@gmail.com>
2022-06-27 15:22:04 -0400

    [FEATURE] optimize image for projects

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ const Skills = ({ skills }) => {
   return (
     <div className='my-2 text-left flex flex-wrap justify-center'>
       {skills.map(skill => (
-        <div className='bg-white text-black font-bold m-2 py-2 px-4 rounded-full' key={skill}>{skill}</div>
+        <div className='bg-white text-black text-sm m-2 py-1 px-2 rounded-full' key={skill}>{skill}</div>
       ))}
     </div>
   )
