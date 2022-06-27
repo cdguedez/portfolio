d6998f4d71c1ef8183cbbd85e7c73f42ed89035f
cdguedez <cdgr.ing@gmail.com>
2022-06-27 08:17:06 -0400

    [DEPLOY] changed bg and palleted colors

--- dist/assets/Contact.ebf9b653.js
+++
@@ -1,1 +0,0 @@
-import{a as t,F as a}from"./index.e3a72b8f.js";const n=()=>t(a,{children:t("h1",{children:"Contact"})});export{n as default};
--- dist/assets/Contact.f24c9eda.js
+++
@@ -1,1 +0,0 @@
-import{a as t,F as a}from"./index.d8570525.js";const n=()=>t(a,{children:t("h1",{children:"Contact"})});export{n as default};
