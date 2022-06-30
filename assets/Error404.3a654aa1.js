9ff0d53cb0d6a6493b5c468022673dd1a9aef115
cdguedez <cdgr.ing@gmail.com>
2022-06-30 16:03:20 -0400

    [DEPLOY] page portfolio test

---
+++ assets/Error404.3a654aa1.js
@@ -0,0 +1,1 @@
+import{a as s,j as r,L as e}from"./index.705d16b8.js";const n=()=>s("section",{children:[r("h1",{className:"",children:"ERROR 404 | PAGE NOT FOUND"}),r(e,{to:"/",children:"Return Home"})]});export{n as default};
